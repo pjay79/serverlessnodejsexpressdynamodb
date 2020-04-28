9bb4377cdb84dcdec8b40858d3b2466acdf5245f
pjay79 <praveen.jayarajan@gmail.com>
2020-04-28 18:00:53 +1000

    add serverless offline and dynamo-db local

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get('/', async (req, res, next) => {
  res.status(200).send('Hello Serverless!')
 })
 
-app.post('/users', function (req, res) {
+app.post('/users/', function (req, res) {
   const { userId, name } = req.body;
   const params = {
     TableName: USERS_TABLE,
@@ -33,23 +33,22 @@ app.post('/users', function (req, res) {
 })
 
 app.get('/users/:userId', function (req, res) {
-  const { userId } = req.params.userId;
   const params = {
     TableName: USERS_TABLE,
     Key: {
-      userId: userId,
+      userId: req.params.userId,
     },
   }
   dynamoDb.get(params, (error, result) => {
     if (error) {
       console.log(error);
-      res.status(400).json({ error: `Could not get user ${userId}` });
+      res.status(400).json({ error: `Could not get user ${req.params.userId}` });
     }
     if (result.Item) {
       const { userId, name } = result.Item;
       res.json({ userId, name });
     } else {
-      res.status(404).json({ error: `User ${userId} not found` });
+      res.status(404).json({ error: `User ${req.params.userId} not found` });
     }
   })
 })
